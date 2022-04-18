bc53b8e5c648e51a8592e21bfcb365dc61351cb5
Zanini <everton.zanini@yahoo.com>
2022-04-18 15:53:45 -0300

    Aula05 - Operadores e Operações, concluída

---
+++ Aula05/Aula05.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Aula05
+{
+    static void Main()
+    {
+        // & = AND / E
+        // | = OR / OU
+        bool res = (5 > 3) | (10 < 5);
+
+        Console.WriteLine(res);
+    }
+}
+
+// using System;
+
+// class Aula05{
+//     static void Main(){
+//         int num = 10;
+//         num = num + 1;
+//         num+=1;
+//         num++;
+
+//         Console.WriteLine(num);
+//     }
+// }
+
+// using System;
+
+// class Aula05{
+//     static void Main(){
+//         bool res = 10 < 5;
+//         Console.WriteLine(res);
+//     }
+// }
