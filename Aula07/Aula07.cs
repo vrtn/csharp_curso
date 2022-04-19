76ed00d3f071012dbcae77ff9aace484b3cbfb19
Zanini <everton.zanini@yahoo.com>
2022-04-19 12:01:29 -0300

    Aula07 - Constantes em C#, concluído

---
+++ Aula07/Aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula07
+{
+    static void Main()
+    {
+        const string canal = "FFB Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal {0} \n PI: {1}", canal,pi);
+    }
+}
