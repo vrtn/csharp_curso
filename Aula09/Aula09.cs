75e151266d4b2e43abf2d17e9d8014b27ee4ddc7
Zanini <everton.zanini@yahoo.com>
2022-04-19 12:28:44 -0300

    Aula09 - Operações de Bitwise, concluída

---
+++ Aula09/Aula09.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Aula09
+{
+    static void Main()
+    {
+        int num = 10;
+
+        Console.WriteLine(num);
+
+        num = num << 1;
+        Console.WriteLine(num);
+
+        num = num >> 1;
+        Console.WriteLine(num);
+    }
+}
