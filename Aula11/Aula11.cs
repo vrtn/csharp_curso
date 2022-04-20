184825b426a6bfbb669629dc233531a4593efda8
Zanini <everton.zanini@yahoo.com>
2022-04-20 09:26:36 -0300

    Aula11 - Conversão de Tipos (typecast), concluída

---
+++ Aula11/Aula11.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Aula11{
+    static void Main(){
+        int vInt = 10;
+        short vShort = (short)vInt;
+        Console.Write(vShort);
+    }
+}
+
+// --------------------------------------------
+// using System;
+
+// class Aula11
+// {
+//     static void Main()
+//     {
+//         float n1 = 10.5f;
+//         int n2 = (int)n1; //type cast
+
+//         Console.WriteLine(n2);
+//     }
+// }
+
+// ---------------------------------------
+// using System;
+
+// class Aula11{
+//     static void Main(){
+//         int n1 = 10;
+//         float n2 = n1;
+//         Console.WriteLine(n2);
+//     }
+// }
