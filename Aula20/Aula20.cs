da37db7b5e9f83b6d818ff719548c84f9c290be6
Zanini <everton.zanini@yahoo.com>
2022-04-25 15:02:11 -0300

    Aula20 - Loop WHILE, concluído

---
+++ Aula20/Aula20.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula19{
+    static void Main(){
+
+        int[] num = new int[10];
+
+        int i = 0;
+        while(i<=10){
+            Console.WriteLine("CFB Cursos {0}",i);
+            i++;
+        }
+
+    }
+}
