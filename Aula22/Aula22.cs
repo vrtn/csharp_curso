80e4a661014e1a4f519e43c1de5d08e5bc2df261
Zanini <everton.zanini@yahoo.com>
2022-04-25 16:53:15 -0300

    Aula22 - FOREACH, concluída

---
+++ Aula22/Aula22.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Aula21{
+    static void Main(){
+        int[] num = new int[3]{11,22,33};
+
+        foreach (int n in num){
+            Console.WriteLine(n);
+        }
+
+    }
+}
