2bb0fe7b1d416548c35bb40a2e1cc3919c84efbb
Zanini <everton.zanini@yahoo.com>
2022-04-25 16:43:07 -0300

    Aula21 - Loop DO WHILE, concluída

---
+++ Aula21/Aula21.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula21{
+    static void Main(){
+
+        int[] n = new int[10];
+
+        int i = 0;
+
+        do{
+            Console.WriteLine("CFB Cursos {0}",i);
+            i++;
+        }while(i<=n.Length);
+    }
+}
