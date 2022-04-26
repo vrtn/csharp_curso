7355e9fb86575b01f7936c1c7527126880624e36
Zanini <everton.zanini@yahoo.com>
2022-04-26 12:50:11 -0300

    Aula25 - Passagem por Valor e Referência, concluída

---
+++ Aula25/Aula25.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aula25{
+    static void Main(){
+        int num = 10;
+        dobrar(ref num);
+        Console.WriteLine(num);
+    }
+
+    static void dobrar(ref int valor){
+        valor *= 2;
+    }
+
+}
