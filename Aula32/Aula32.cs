7d00e5e4393d5157cac0c6dd2cdaf4c46c38a096
Zanini <everton.zanini@yahoo.com>
2022-04-28 14:36:42 -0300

    Aula32 - This, concluída

---
+++ Aula32/Aula32.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Calculos
+{
+    public int v1;
+
+    public int v2;
+
+    public Calculos(int v1, int v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public int Somar()
+    {
+        return v1 + v2;
+    }
+}
+
+class Aula32
+{
+    static void Main()
+    {
+
+        int res;
+
+        Calculos c = new Calculos(10, 2);
+
+        res = c.Somar();
+
+        Console.WriteLine(res);
+    }
+}
