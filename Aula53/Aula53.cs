29cfed089a34cd6b12c2c1f0110d15a54e1f3f45
Zanini <everton.zanini@yahoo.com>
2022-05-09 11:29:54 -0300

    Aula53 - Try, Catch, Finally parte 2, concluída

---
+++ Aula53/Aula53.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Area
+{
+    public static double quadrado(double largura, double altura)
+    {
+        if (largura == 0 || altura == 0)
+        {
+            throw new Exception("Valores precisam ser maiores que zero...");
+        }
+        return largura * altura;
+    }
+}
+
+class Aula53
+{
+    static void Main()
+    {
+        double area = 0;
+
+        try
+        {
+            area = Area.quadrado(10.0, 0.0);
+            Console.WriteLine("Área do Quadrado: {0}m²", area);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("ERRO........: {0}", e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Fim do processo.");
+            // area = Area.quadrado(10.0, 5.0);
+            // Console.WriteLine("Área do Quadrado: {0}m²", area);
+        }
+
+    }
+}
